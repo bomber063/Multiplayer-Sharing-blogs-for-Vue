eb5da87c6727c2b2c51193cc6d3f8a4f19ff4dcc
bomber <tangyihong063@163.com>
2020-06-14 19:36:42 +0800

    后端返回的博客id为1337的用户（user）不存在，为null，所以增加三元运算符判断用户是否存在

--- blog-client/src/pages/Index/template.vue
+++ blog-client/src/pages/Index/template.vue
@@ -6,8 +6,10 @@
       <!-- 这里的错误的原因，第一是blogId写成了可blodId，手抖。第二个是blogs里面是一个数组，需要获取到某一个数组后才可以获取到数组的id，所以这里最好还是用for循环的blog来获取id比较合适 -->
       <router-link  v-for="blog of blogs" :key="blog.id" :to="`/detail/${blog.id}`" class="item">
         <figure class="avatar">
-          <img :src="blog.user.avatar" :alt="blog.user.username">
-          <figcaption>{{blog.user.username}}</figcaption> 
+          <!-- <img :src="blog.user.avatar" :alt="blog.user.username"> -->
+          <!-- <figcaption>{{blog.user.username}}</figcaption>  -->
+          <img :src="blog.user?blog.user.avatar:'用户不存在'" :alt="blog.user?blog.user.username:'用户不存在'">
+          <figcaption>{{blog.user?blog.user.username:'用户不存在'}}</figcaption> 
         </figure>
         <h3>{{blog.title}} <span> {{blog.updatedAt}}</span></h3> 
         <p>{{blog.description}}</p>
