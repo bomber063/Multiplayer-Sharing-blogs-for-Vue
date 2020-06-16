4202d39acf81132c0bc1ac0f4013daf9f7e211d2
bomber <tangyihong063@163.com>
2020-06-16 13:05:02 +0800

    路由目录的错误修改

--- blog-client/src/components/header.vue
+++ blog-client/src/components/header.vue
@@ -19,8 +19,8 @@
       <h1>Let's share</h1>
       <p>精品博客汇聚</p>
       <div class="btns">
-        <router-link to="./login"><el-button>立即登录</el-button></router-link>
-        <router-link to="./register"><el-button>注册账号</el-button></router-link>
+        <router-link to="/login"><el-button>立即登录</el-button></router-link>
+        <router-link to="/register"><el-button>注册账号</el-button></router-link>
       </div>
     </template>
     <template v-if="isLogin">
